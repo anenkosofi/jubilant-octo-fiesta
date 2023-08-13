bf5a1c9ac67eadf19ab66f2d13b17aa92f26eff0
Sofiia Anenko <sofiiaa@oddity.com>
2023-08-13 21:14:43 +0300

    fix: controller wrapper

--- controllers/products/getProducts.js
+++ controllers/products/getProducts.js
@@ -1,11 +1,8 @@
 const { Product } = require('../../models');
+console.log('hehe');
 
 const getProducts = async (req, res) => {
-  const { page = 1, limit = 10, } = req.query;
-  const products = await Product.find({}, '', {
-    skip: (page - 1) * limit,
-    limit: Number(limit),
-  });
+  const products = await Product.find({});
   res.json(products);
 };
 
--- utils/controller.js
+++ utils/controller.js
@@ -1,7 +1,7 @@
 const controller = ctrl => {
   const f = async (req, res, next) => {
     try {
-      await controller(req, res, next);
+      await ctrl(req, res, next);
     } catch (error) {
       next(error);
     }
