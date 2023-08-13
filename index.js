474a84f243259e1207b142e6d6e1d4c5f23c578d
Sofiia Anenko <sofiiaa@oddity.com>
2023-08-13 16:36:02 +0300

    init: set up project

---
+++ index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const app = require('./app');
+
+const { DB_HOST, PORT = 8080 } = process.env;
+
+mongoose.set('strictQuery', false);
+
+mongoose
+    .connect(DB_HOST)
+    .then(
+        app.listen(PORT, () => {
+            console.log(`Server running. Use our API on port: ${PORT}`);
+        })
+    )
+    .catch(error => {
+        console.log(error.message);
+        process.exit(1);
+    });
